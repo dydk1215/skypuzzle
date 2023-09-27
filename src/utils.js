46e82b358a2d83b1026824ea27ce60a7beda2015
jbilee <128875051+jbilee@users.noreply.github.com>
2023-09-27 19:09:35 +0900

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
-export const create = (tag, className) => {
+const create = (tag, className) => {
     const el = document.createElement(tag);
     if (className) el.classList.add(className);
     return el;
 }
 
-export const getRandNums = (max) => {
+const getRandNums = (max) => {
     const randnums = [];
     while (randnums.length < (max - 1)) {
         const randnum = Math.floor(Math.random() * (max - 1));
@@ -12,3 +12,23 @@ export const getRandNums = (max) => {
     }
     return randnums;
 }
+
+const checkValidity = (array) => {
+    let count = 0;
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] > array[j]) count++;
+        }
+    }
+    return count % 2 === 0;
+}
+
+const shuffleOrder = () => {
+    let randnums;
+    do {
+        randnums = getRandNums(CAP);
+    } while (!checkValidity(randnums))
+    return randnums;
+}
+
+module.exports = { create, getRandNums, checkValidity, shuffleOrder }
