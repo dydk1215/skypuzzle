004df96f87b90a72f2ea49372f214fd0ddc6a078
jbilee <128875051+jbilee@users.noreply.github.com>
2023-09-28 12:56:35 +0900

    Update constants.js
    Include grid items as a constant

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@ const XTILES = 3;
 const YTILES = 3;
 const CAP = 9;
 const grid = document.querySelector(".grid");
+const tiles = grid.children;
 const pictures = [
     ["./img/p01/0x0.jpg", "./img/p01/1x0.jpg", "./img/p01/2x0.jpg", "./img/p01/0x1.jpg", "./img/p01/1x1.jpg", "./img/p01/2x1.jpg", "./img/p01/0x2.jpg", "./img/p01/1x2.jpg", "./img/p01/2x2.jpg"],
     ["./img/p02/0x0.jpg", "./img/p02/1x0.jpg", "./img/p02/2x0.jpg", "./img/p02/0x1.jpg", "./img/p02/1x1.jpg", "./img/p02/2x1.jpg", "./img/p02/0x2.jpg", "./img/p02/1x2.jpg", "./img/p02/2x2.jpg"],
@@ -13,4 +14,4 @@ const pictures = [
     ["./img/p08/0x0.jpg", "./img/p08/1x0.jpg", "./img/p08/2x0.jpg", "./img/p08/0x1.jpg", "./img/p08/1x1.jpg", "./img/p08/2x1.jpg", "./img/p08/0x2.jpg", "./img/p08/1x2.jpg", "./img/p08/2x2.jpg"]
 ];
 
-module.exports = { XTILES, YTILES, CAP, grid, pictures }
+module.exports = { XTILES, YTILES, CAP, grid, pictures, tiles }
