467ed107c080fa4614b60214ff24c2d1fbcb50cf
jbilee <128875051+jbilee@users.noreply.github.com>
2023-09-28 13:30:46 +0900

    Update Game.js
    Refactor: Cut down on unnecessary imports

--- src/Game.js
+++ src/Game.js
@@ -1,37 +1,7 @@
-import { XTILES, YTILES, CAP, grid, pictures, tiles } from "./constants.js"
-import { create, getRandNums, checkValidity, shuffleOrder, getCoords, getImgURL } from "./utils.js"
+import { CAP, grid, pictures, tiles } from "./constants.js"
+import { getCoords, getImgURL } from "./utils.js"
 
-class NewGame {
-    constructor(id) {
-        this.drawBoard();
-        this.shuffleImg(id);
-        this.game = new Game(id);
-    }
-
-    drawBoard() {
-        for (let i = 0; i < YTILES; i++) {
-            for (let j = 0; j < XTILES; j++) {
-                const tile = create("div");
-                tile.classList.add("tile", `x${j}`, `y${i}`);
-                if (i === (YTILES - 1) && j === (XTILES - 1)) tile.classList.add("empty");
-                tile.addEventListener("click", (e) => {
-                    // e.preventDefault();
-                    this.game.makeMove();
-                });
-                grid.append(tile);
-            }
-        }
-    }
-
-    shuffleImg(id) {
-        const randnums = shuffleOrder();
-        for (let i = 0; i < tiles.length - 1; i++) {
-            tiles[i].style.backgroundImage = `url(${pictures[id][randnums[i]]})`;
-        }
-    }
-}
-
-class Game {
+export default class Game {
     constructor(id) {
         this.id = id;
         this.answer = this.getAnswer(id);
@@ -89,5 +59,3 @@ class Game {
         console.log("winner!");
     }
 }
-
-export = { NewGame, Game }
